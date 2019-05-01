7c3657c72630fdf7d1c51e6faec4c092608ab2f7
Bodera <rafaelrafa990@gmail.com>
2019-04-30 23:43:11 -0300

    test suit completed.

--- [Course] -- NodeBR/06-cli/database.js
+++ [Course] -- NodeBR/06-cli/database.js
@@ -1,11 +1,13 @@
 const {
-    readFile
+    readFile,
+    writeFile
 } = require('fs') //um callback??!
 const {
     promisify
 } = require('util') //teamPromisse!!
 
 const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
 
 class Database {
 
@@ -18,16 +20,65 @@ class Database {
             const arquivo = await readFileAsync(__dirname + '/' + this.NOME_ARQUIVO, 'utf8')
             return JSON.parse(arquivo.toString())
         }
-        escreverArquivo() {
-
+        async escreverArquivo(dados) {
+            await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+            return true
         }
 
-        //método princial da classe
+        //métodos principais da classe
         async listar(id) {
             const dados = await this.obterDadosArquivo()
             const dadosFiltrados = dados.filter(item => (id ? (item.id === id) : true)) //caso nenhum id seja passado ao chamar a função, traga todos ids.
             
             return dadosFiltrados
+       }
+       async cadastrar(heroi) {
+           const dados = await this.obterDadosArquivo()
+           //cria um objeto que será concatenado aos dados do heroi para identificação
+           const id = heroi.id <= 2 ? heroi.id : Date.now(); //Propósito educacional e não ilustra as melhores práticas.
+
+           const heroiComId = {
+               id,
+               ...heroi //MAGIC! Concatenando 2 objetos em 1
+           }
+           const dadosFinal = [
+               ...dados,
+               heroiComId //MAGIC! Concatenando 1 array com 1 objeto
+           ]
+           const resultado = await this.escreverArquivo(dadosFinal)
+           return resultado;
+       }
+       async remover(id) {
+           if(!id) { //nenhum id foi passado
+               return await this.escreverArquivo() //dropa tudo do arquivo. Um array vazio
+           }
+           const dados = await this.obterDadosArquivo() //procura id na lista
+           const indice = dados.findIndex(item => item.id === parseInt(id, 10)) //conseguimos passar função diferente do indexOf
+
+           if(indice === -1) { //não achou nenhum id
+                throw Error('O herói informado não existe')
+           }
+           dados.splice(indice, 1) //a partir do meu índice delete um único item.
+           return await this.escreverArquivo(dados) //após deletar reescreva o arquivo
+       }
+       async atualizar(id, modificacoes) { //identificador do objeto e campos alterados
+            const dados = await this.obterDadosArquivo() 
+            const indice = dados.findIndex(item => item.id === parseInt(id, 10))
+            if(indice === -1) {
+                throw Error('O herói informado não existe.')
+            }
+
+            const atual = dados[indice]
+            const target = {
+                ...atual,
+                ...modificacoes
+            }
+            dados.splice(indice, 1)
+
+            return await this.escreverArquivo([
+                ...dados,
+                target
+            ])
        }
 }
 
--- [Course] -- NodeBR/06-cli/test.js
+++ [Course] -- NodeBR/06-cli/test.js
@@ -7,16 +7,26 @@ const {
 
 const database = require('./database')
 
+//cria objeto padrão para operações de leitura, escrita e remoção
 const DEFAULT_ITEM_CADASTRAR = {
     nome: 'Flash',
     poder: 'Super-velocidade',
     id: 1
 }
 
+//cria objeto padrão para operações de atualização de arquivos
+const DEFAULT_ITEM_ATUALIZAR = {
+    nome: 'Lanterna Verde',
+    poder: 'Energia do anel',
+    id: 2
+}
 
 //Uma suíte de testes é uma coleção de casos de teste ou specs destinados a testar um programa para verificar um determinado comportamento.
 describe('Suíte de manipulação de Heróis', () => {
-
+    before(async () => { //Não deixa a base de dados vazia, prevenindo de tentar excluir algo que não exista.
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        await database.cadastrar(DEFAULT_ITEM_ATUALIZAR)
+    })
     it('deve pesquisar um herói usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR //presumi-se que ao menos 1 registro tenha sido previamente cadastrado.
         const [resultado] = await database.listar() //aplicando destructing
@@ -24,10 +34,33 @@ describe('Suíte de manipulação de Heróis', () => {
         
         deepEqual(resultado, expected)
     })
+    it('deve cadastrar um herói usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR) //poderia ser outro modelo
+        const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id) //conferir se o modelo já está cadastrado
 
-    //it('deve cadastrar um herói usando arquivos', async () => {
-    //    const expected = { DEFAULT_ITEM_CADASTRAR }
-    //    //
-    //    ok(null, expected)
-    //})
+        deepEqual(actual, expected)
+    })
+    it('deve atualizar um herói pelo id', async () => { //a ordem de execução dos testes importa
+        const expected_structure = {
+            ...DEFAULT_ITEM_ATUALIZAR, //concatenando objetos
+            nome: 'Caçador de Marte',
+            poder: 'Telepatia'
+        }
+        const expected_input = {
+            nome: 'Caçador de Marte',
+            poder: 'Telepatia'
+        }
+        
+        await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, expected_input)
+        const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id)
+        deepEqual(resultado, expected_structure)
+    })
+    //run it.only to perfom only one assert. >mocha test.js
+    it('deve remover um herói por id', async () => {
+        const expected = true
+        const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id)
+        
+        deepEqual(resultado, expected)
+    })
 })

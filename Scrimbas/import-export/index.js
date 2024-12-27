92bde6bd69296283ed6162d9a9f4b76e021c590a
Bode <rafaelrafa990@gmail.com>
2024-12-26 22:18:16 -0300

    upload export default at import-export

--- Scrimbas/import-export/index.js
+++ Scrimbas/import-export/index.js
@@ -8,9 +8,15 @@ import {
     shortSpaceTripsArr as shortTripFromMoreThanOnce
 } from './exporting-more-than-once.js'
 
+import getMatchingTripsArr from './getMatchingTripsArr.js'
+
 console.log(longTripFromJustOnce.length)
 console.log(shortTripFromJustOnce.length)
 
 
 console.log(longTripFromMoreThanOnce.length)
 console.log(shortTripFromMoreThanOnce.length)
+
+
+console.log(getMatchingTripsArr(shortTripFromMoreThanOnce, 'moon'))
+console.log(getMatchingTripsArr(longTripFromJustOnce, 'moon'))

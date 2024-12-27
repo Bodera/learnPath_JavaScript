951e4416f69eff691e11baea4ffc9a0b816f3e3b
Bode <rafaelrafa990@gmail.com>
2024-12-26 22:01:17 -0300

    upload for-each

---
+++ Scrimbas/forEach/index.js
@@ -0,0 +1,43 @@
+/* 
+    Below is an array of objects representing expenses and refunds. 
+    Positive amounts are expenses, negative amounts are refunds. 
+    
+    We want to find out how much money was spent in 2023. 
+*/
+
+const expensesAndRefunds = [
+    { description: 'Rent', amount: -500, year: 2024 },
+    { description: 'Utilities', amount: 100, year: 2024 },
+    { description: 'Books', amount: 20, year: 2024 },
+    { description: 'Fitness', amount: 30, year: 2024 },
+    { description: 'Gifts', amount: 15, year: 2024 },
+    { description: 'Groceries', amount: 50, year: 2023 },
+    { description: 'Electronics', amount: -30, year: 2023 },
+    { description: 'Dinner', amount: 40, year: 2023 },
+    { description: 'Clothing', amount: 60, year: 2023 },
+    { description: 'Entertainment', amount: 25, year: 2023 }
+]
+
+// Sort the expenses and refunds by year so we can use break and continue without worrying about order 
+expensesAndRefunds.sort((a, b) => a.year - b.year)
+
+let totalSpent = 0
+const cutoffDate = 2024
+
+for (let currentExpenseOrRefund of expensesAndRefunds) {
+    
+    if (currentExpenseOrRefund.year >= cutoffDate) {
+        console.log(`Reached cutoff date, exiting loop`)
+        break   
+    }
+    
+    if (currentExpenseOrRefund.amount < 0) {
+        console.log(`Skipping ${currentExpenseOrRefund.description} due to refund`)
+        continue
+    } 
+    
+    totalSpent += currentExpenseOrRefund.amount 
+}
+
+
+console.log(`Total amount spent on items in 2023: $${totalSpent}`)

cf9d24a85aedb413f268ed8bcfe2ae4659fe2713
Bode <rafaelrafa990@gmail.com>
2024-12-28 11:17:54 -0300

    upload try-catch

---
+++ Scrimbas/try-catch/index.js
@@ -0,0 +1,14 @@
+function addTouristSurcharge(payment) {
+    try {
+        if (typeof payment === 'number') {
+            console.log(payment + 10)
+        } else {
+            throw new ReferenceError('payment is not a number')
+        }
+    } catch (err) {
+        console.error('Error: ' + err)
+    }
+}
+
+addTouristSurcharge(60)
+addTouristSurcharge('60')
